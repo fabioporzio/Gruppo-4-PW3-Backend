784c08e1ecf1e30a3cb9e6073bd7b91b0e104808
RosG-20 <rosgiugno20@gmail.com>
2025-05-28 15:06:34 +0200

    added the method to load the profile of the users

---
+++ src/main/java/web/ProfileResource.java
@@ -0,0 +1,39 @@
+package web;
+
+import data.repository.PersonRepository;
+import jakarta.annotation.security.DenyAll;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.jwt.JsonWebToken;
+import service.PersonService;
+
+@Path("/profile")
+@DenyAll
+public class ProfileResource {
+    private final PersonService personService;
+    private final JsonWebToken jwtToken;
+
+    public ProfileResource(PersonService personService, JsonWebToken jwtToken) {
+        this.personService = personService;
+        this.jwtToken = jwtToken;
+    }
+
+    @GET
+    @RolesAllowed({"Admin", "Requester", "Reception"})
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response getProfile() {
+        Object idClaim = jwtToken.getClaim("id");
+        int id = Integer.parseInt(idClaim.toString());
+
+        System.out.println("id: " + id);
+
+        return Response.ok(personService.getPersonById(id)).build();
+    }
+
+}
